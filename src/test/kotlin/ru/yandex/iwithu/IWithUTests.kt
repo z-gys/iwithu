3bcfb36aaab318eef98ac2cbfa1fe21bb71b9b8e
ugoryntsev <ugoryntsev@yandex-team.ru>
2022-01-27 14:19:37 +0300

    Initial commit

---
+++ src/test/kotlin/ru/yandex/iwithu/IWithUTests.kt
@@ -0,0 +1,13 @@
+package ru.yandex.iwithu
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class IWithUTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
